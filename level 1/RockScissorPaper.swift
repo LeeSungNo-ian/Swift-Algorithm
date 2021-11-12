891ee29b98d92e57f4b167e8bf7a69c00bb530b1
ian <78950704+LeeSungNo-ian@users.noreply.github.com>
2021-11-12 22:57:40 +0900

    [#1]feat: myChoicePick case별 구분
    myChoicePick case별 구분

--- level 1/RockScissorPaper.swift
+++ level 1/RockScissorPaper.swift
@@ -10,4 +10,14 @@ import Foundation
 var myChoicePick: Int = 0
 var computerChoicePick: Int = Int.random(in: 0...2)
 
+switch myChoicePick {
+case 0:
+    print("사용자는 가위를 냈습니다.")
+case 1:
+    print("사용자는 바위를 냈습니다.")
+case 2:
+    print("사용자는 보를 냈습니다.")
+default:
+    break
+}
 
