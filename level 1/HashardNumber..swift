61b3873de5d90093ae269e2e07a066f6855bf8a3
ian <78950704+LeeSungNo-ian@users.noreply.github.com>
2021-11-16 16:42:08 +0900

    [#1]feat: Int 타입 String 타입으로 변환
    Int 타입 String 타입으로 변환

---
+++ level 1/HashardNumber..swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HashardNumber.
+//
+//  Created by 이성노 on 2021/11/16.
+//
+
+import Foundation
+
+func solution(_ x: Int) -> Bool {
+    let stringNumber: String = String(x)
+}
